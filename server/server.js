bf83712730ef32a25f86f6c0749fc9d1ab2ba8e1
Abhishek Agrawal <abhishekagrawal907@gmail.com>
2020-06-05 13:32:14 +0530

    save index.html and corrected the publichpath using / instead of "

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const express = require('express')
-const publicPath = path.join(__dirname,'\..\public');
+const publicPath = path.join(__dirname,'/../public');
 const port = process.env.PORT || 3000
 const app = express();
 
 app.use(express.static(publicPath));
-app.set('view engine', 'html');
+
 
 app.get('/',function(req,res){
     res.status(200);
     res.render('index');
 })
+
 app.listen(port,(err)=>{ 
     if(err){ 
         console.log("Server could not be stareted due to the error : " + err)
